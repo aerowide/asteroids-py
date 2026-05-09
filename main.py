d311791eaed179a37ed02c0e4ce14bf8cab83e27
aerowide <sadman0110@gmail.com>
2026-05-09 16:02:20 +0300

    baby steps

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
 import pygame
+from logger import log_state
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 
 def main():
+    # initialize pygame
+    pygame.init()
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
+    
+    # set screen size
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
+    
+    while True:
+        log_state()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return;
+
+        screen.fill("black")
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
